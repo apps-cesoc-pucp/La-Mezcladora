6b06fdd3124a7d694c604942e9670198d2b79c8d
Jean Carlos Lima <Charlo@Charlo.local>
2018-12-19 14:28:06 -0500

    correccion global

--- lib/activities/pag3.dart
+++ lib/activities/pag3.dart
@@ -1,25 +1,42 @@
 import 'package:flutter/material.dart';
+import 'pag3_v2.dart';
 import '../aux/mybutton.dart';
 
-class Pag3 extends StatelessWidget {
+class Pag3 extends StatefulWidget {
+  
   // This widget is the root of your application.
+  @override
+  State createState() => new Pag3State();
+}
+class Pag3State extends State<Pag3>{
+  bool overlayOn;
+  @override
+  void initState() {
+    super.initState();
+    overlayOn=false;
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'La Mezcladora',
+      title: "La Mezcladora",
       home:Scaffold(
         appBar: AppBar(
-          title: Text('La Mezcladora'),
+          title: Text("La Mezcladora"),
         ), 
-        body: Padding(
-          padding: EdgeInsets.only(top:40),
-          child:Column(          
+        body: Stack(
+          //
+          
+          fit: StackFit.expand,
+          children:<Widget>[
+            Padding(
+            padding: EdgeInsets.only(top:40),
+            child:Column(          
             children: <Widget> [
             Padding(
-              padding: EdgeInsets.only(left:70, top:20),
+              padding: EdgeInsets.only(top:20),
               child: Text(
-                'MORTERO PARA:',
+                "MORTERO PARA:",
                 style: TextStyle(
                   fontSize: 30,
                   color: Colors.black,
@@ -27,18 +44,54 @@ class Pag3 extends StatelessWidget {
                 ),
               ),
               ),
+           
             Padding(
-              padding: EdgeInsets.only(left: 55,top:100),
-              child: MyButton(name:'LADRILLO PANDERETA',width: 100,height: 100,),
+              padding: EdgeInsets.only(top:50),
+              child: ButtonTheme(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100/3)),
+                minWidth: 100,
+                height: 80,
+
+                child: RaisedButton(
+                 onPressed: () {overlayOn=true;print(overlayOn);},
+                 color: Color.fromRGBO(255, 193, 7,1),
+                 
+                 child: Text(
+                   "LADRILLO PANDERETA",
+                   style: TextStyle(
+                     color: Colors.black,
+                     fontSize: 25,
+                    ),
+                ),
+                ),
+            ),
             ),
             Padding(
-              padding: EdgeInsets.only(left: 55,top:150),
-              child: MyButton(name:'LADRILLO KING KONG',width: 100,height: 100),
+              padding: EdgeInsets.only(top:270),
+              child: MyButton(name:"LADRILLO KING KONG",width: 100,height: 80),
             ),            
-            ]
-          ),
+            
+            ]),
         ),
+        overlayOn==true ? new Container(
+          color: Colors.black38,
+          child: Column(
+            children: <Widget>[
+              Padding(padding: EdgeInsets.only(top:250),),
+              MyButton(name: "SOGA",width: 150,height: 80),
+              Padding(padding: EdgeInsets.only(top:20),),
+              MyButton(name: "CANTO",width: 150,height: 80),
+            ],
+          )
+        
+        
+        ):new Container(),
+        ]
+      )
       )
     ); 
   }
 }
+
+
+
---
+++ lib/activities/pag3_v2.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class OverlayOn extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      width: 300,
+      height: 300,
+      color: Colors.black,
+      child: new Container(
+        //onTap: () => print('hola'),
+        /*child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Container(
+              
+            ),
+            new Padding(
+              padding: new EdgeInsets.only(bottom: 20.0),
+            ),
+            
+          ],
+        ),*/
+      ),
+    );
+  }
+}
